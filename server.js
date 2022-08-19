d8cbad8a1f4d62ec79a952e3b2aaab2bc95b067c
Sylvester Brandon <sylvester.brandon25@gmail.com>
2022-08-19 16:34:01 -0400

    update and completed coded corrections

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+const apiRoutes = require('./routes/apiRoutes/api');
+const htmlRoutes = require('./routes/htmlRoutes/view');
 
+app.use(express.static('public'));
+// parse incoming string or array data
+app.use(express.urlencoded({extended: true}));
+// parse incoming JSON data
+app.use(express.json());
 
-//Port listen on
-const PORT = process.env.PORT || 3000
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-//Reading static file inside public folders
-app.use(express.static(__dirname + '/public'));
-
-//Body parsor
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
-
-require("./routes/api")(app);
-require("./routes/view")(app);
-
-
-//Port listening on
 app.listen(PORT, () => {
-    console.log(`PORT Listening ON ${PORT}`)
-})
+    console.log(`API server now on port ${PORT}. Welcome!`);
+  });
